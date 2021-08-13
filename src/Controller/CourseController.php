e40920d9ce06dfd33bb10dba6e2139e9f7c2992e
ay0ubamj <ayoubamaj410@gmail.com>
2021-08-13 22:39:49 +0100

    Participer à une formation

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,22 +2,25 @@
 
 namespace App\Controller;
 
+use DateTimeImmutable;
 use App\Entity\Formation;
-use App\Entity\Commentaire;
 use App\Form\CommentsType;
-use Knp\Component\Pager\PaginatorInterface;
+use App\Entity\Commentaire;
+use App\Form\SubscribeCourseFormType;
+use App\Form\UnsubscribeCourseFormType;
 use App\Repository\FormationRepository;
 use App\Repository\CommentaireRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CourseController extends AbstractController
 {
     #[Route('/course/{slug}', name: 'course')]
-    public function index(Formation $formation, 
+    public function index(
+        Formation $formation,
         CommentaireRepository $commentaireRepository,
         PaginatorInterface $paginator,
         Request $request
@@ -32,12 +35,13 @@ class CourseController extends AbstractController
         // Création du formulaire de commentaire
         $comment = new Commentaire;
         $date = new DateTimeImmutable();
+        $user = $this->getUser();
 
         $commentForm = $this->createForm(CommentsType::class, $comment);
         $commentForm->handleRequest($request);
 
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $user = $this->getUser();
+
             $comment->setCommentaire($commentForm->get('commentaire')->getData());
             $comment->setCreatedAt($date);
             $comment->setEtat(false);
@@ -52,12 +56,53 @@ class CourseController extends AbstractController
             return $this->redirectToRoute('course', ['slug' => $formation->getSlug()]);
         }
 
+        // Users in the course
+        $courseUsers = $formation->getUser();
+
+        // SubscribeInCourse
+        $SubcourseForm = $this->createForm(SubscribeCourseFormType::class, $formation);
+        $SubcourseForm->handleRequest($request);
+
+        if ($SubcourseForm->isSubmitted() && $SubcourseForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user->addFormation($formation);
+            $formation->addUser($user);
+
+            $em->persist($user);
+            $em->persist($formation);
+            $em->flush();
+
+            $this->addFlash('message', 'You are registred now in this course !');
+            return $this->redirectToRoute('course', ['slug' => $formation->getSlug()]);
+        }
+
+        // UnSubscribeInCourse
+        $UnSubcourseForm = $this->createForm(UnsubscribeCourseFormType::class, $formation);
+        $UnSubcourseForm->handleRequest($request);
+
+        if ($UnSubcourseForm->isSubmitted() && $UnSubcourseForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user->removeFormation($formation);
+            $formation->removeUser($user);
+
+            $em->persist($user);
+            $em->persist($formation);
+            $em->flush();
+
+            $this->addFlash('message', 'You are no longer registred in this course !');
+            return $this->redirectToRoute('course', ['slug' => $formation->getSlug()]);
+        }
 
         return $this->render('course/index.html.twig', [
             'formation' => $formation,
             'commentaires' => $commentaires,
             'nbrCommentaires' => $commentaireRepository->NumberofComments($formation),
             'commentForm' => $commentForm->createView(),
+            'SubForm' => $SubcourseForm->createView(),
+            'UnSubForm' => $UnSubcourseForm->createView(),
+            'courseUsers' => $courseUsers,
         ]);
     }
 }
